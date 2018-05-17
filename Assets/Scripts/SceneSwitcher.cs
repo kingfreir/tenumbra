a284ea569ab680c23c97fdd550f3b4a38898752e
Ricardo Rodrigues <2080414@student.uma.pt>
2018-05-17 10:53:25 +0100

    Add scene change

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void GotoTitleScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
